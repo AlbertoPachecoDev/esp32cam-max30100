aadaabf16c18cb2fa6cc4ce791b2488f560e5e7a
Alberto Pacheco <80423661+AlbertoPachecoDev@users.noreply.github.com>
2022-04-04 13:45:08 -0600

    Update esp32cam-max30100-basic_reading.ino

--- esp32cam-max30100-basic_reading/esp32cam-max30100-basic_reading.ino
+++ esp32cam-max30100-basic_reading/esp32cam-max30100-basic_reading.ino
@@ -7,11 +7,11 @@
   Outputs all Red/IR/Green values.
 
   Hardware Connections (Breakoutboard to Arduino):
-  -VCC = -3.3V 
-  -GND = GND
-  -SDA = A4 (or SDA) + 4.7Kohms a Vcc (pull-up)
-  -SCL = A5 (or SCL) + 4.7Kohms a Vcc (pull-up)
-  **-INT = Not connected** TO-DO
+  - VCC = 3.3V (Usar de preferencia 3.3v, en vez de 5v) 
+  - GND = GND
+  - SDA = A4 (or SDA) + 4.7Kohms a Vcc (pull-up)
+  - SCL = A5 (or SCL) + 4.7Kohms a Vcc (pull-up)
+  - INT = Not connected (TO-DO)
 
   The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
   but it will also run at 3.3V.
@@ -19,7 +19,7 @@
   This code is released under the [MIT License](http://opensource.org/licenses/MIT).
 */
 
-//#include <Wire.h>
+// #include <Wire.h> // No es necesario, la librería del sensor lo incluye  
 #include "MAX30105.h"
 
 MAX30105 particleSensor;
@@ -35,7 +35,7 @@ void setup()
     Serial.println("MAX30102 was not found. Please check wiring/power.");
     return; // while(1);
   }
-  particleSensor.setup(); // Configure sensor. Use 6.4mA for LED drive
+  particleSensor.setup(); // Configure sensor: investigar los parámetros posibles
 }
 
 void loop() {
@@ -43,8 +43,8 @@ void loop() {
   Serial.print(particleSensor.getRed());
   Serial.print("] IR[");
   Serial.print(particleSensor.getIR());
-  // NO EXISTE EN EL SENSOR MAX301102 Serial.print("] G[");
-  // NO EXISTE EN EL SENSOR MAX301102 Serial.print(particleSensor.getGreen());
+  // EL SENSOR MAX30102 NO TIENEN LED GREEN Serial.print("] G[");
+  // EL SENSOR MAX30102 NO TIENEN LED GREEN Serial.print(particleSensor.getGreen());
   Serial.print("]");
   Serial.println();
   delay(2000);
